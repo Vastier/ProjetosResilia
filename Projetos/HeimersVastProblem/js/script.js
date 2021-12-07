96f49d66a630bf753450c506ad5a7a2526ac28ed
Iago Viana <iago0.viana@gmail.com>
2021-12-06 23:13:38 -0300

    Commit Inicial do Portfólio.

---
+++ Projetos/HeimersVastProblem/js/script.js
@@ -0,0 +1,153 @@
+
+function docshisCena1Escolha() {
+	var cena1escolha = ""
+
+	while (true) {
+		cena1escolha = prompt('Escolha 1 ou 2')
+
+		if (cena1escolha == 1) {
+			window.location.href = "./startlose.html"
+		} else if (cena1escolha == 2) {
+			window.location.href = "./middle.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function docshisCena2Escolha() {
+	var cena2escolha = ""
+
+	while (true) {
+		cena2escolha = prompt('Escolha 1 ou 2')
+
+		if (cena2escolha == 1) {
+			window.location.href = "./end.html"
+		} else if (cena2escolha == 2) {
+			window.location.href = "./middlelose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function docshisCena3Escolha() {
+	var cena3escolha = ""
+
+	while (true) {
+		cena3escolha = prompt('Escolha 1 ou 2')
+
+		if (cena3escolha == 1) {
+			window.location.href = "./endwin.html"
+		} else if (cena3escolha == 2) {
+			window.location.href = "./endlose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function platypCena1Escolha() {
+	var cena1escolha = ""
+
+	while (true) {
+		cena1escolha = prompt('Escolha 1 ou 2')
+
+		if (cena1escolha == 1) {
+			window.location.href = "./middle.html"
+		} else if (cena1escolha == 2) {
+			window.location.href = "./startlose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function platypCena2Escolha() {
+	var cena2escolha = ""
+
+	while (true) {
+		cena2escolha = prompt('Escolha 1 ou 2')
+
+		if (cena2escolha == 1) {
+			window.location.href = "./end.html"
+		} else if (cena2escolha == 2) {
+			window.location.href = "./middlelose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function platypCena3Escolha() {
+	var cena3escolha = ""
+
+	while (true) {
+		cena3escolha = prompt('Escolha 1 ou 2')
+
+		if (cena3escolha == 1) {
+			window.location.href = "./endwin.html"
+		} else if (cena3escolha == 2) {
+			window.location.href = "./endlose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function pengCena1Escolha() {
+	var cena1escolha = ""
+
+	while (true) {
+		cena1escolha = prompt('Escolha 1 ou 2')
+
+		if (cena1escolha == 1) {
+			window.location.href = "./startlose.html"
+		} else if (cena1escolha == 2) {
+			window.location.href = "./middle.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function pengCena2Escolha() {
+	var cena2escolha = ""
+
+	while (true) {
+		cena2escolha = prompt('Escolha 1 ou 2')
+
+		if (cena2escolha == 1) {
+			window.location.href = "./middlelose.html"
+		} else if (cena2escolha == 2) {
+			window.location.href = "./end.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
+
+function pengCena3Escolha() {
+	var cena3escolha = ""
+
+	while (true) {
+		cena3escolha = prompt('Escolha 1 ou 2')
+
+		if (cena3escolha == 1) {
+			window.location.href = "./endwin.html"
+		} else if (cena3escolha == 2) {
+			window.location.href = "./endlose.html"
+		} else { 
+			alert("Certeza que colocou uma opção válida?")
+		}
+	break
+	}
+}
